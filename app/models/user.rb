109f886959607f084e1ce114027997752764fdec
Gurshan Sekhon <gsekhon41@rrc.ca>
2026-04-08 12:51:40 -0500

    Add ransackable_attributes and associations to User for ActiveAdmin filtering

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,13 @@ class User < ApplicationRecord
     format: { with: /\A[A-Za-z]\d[A-Za-z][ -]?\d[A-Za-z]\d\z/,
               message: "must be a valid Canadian postal code" },
     allow_blank: true
+
+  # Ransack configuration for Active Admin filters
+  def self.ransackable_attributes(auth_object = nil)
+    %w[id name email address city postal_code province_id created_at updated_at]
+  end
+
+  def self.ransackable_associations(auth_object = nil)
+    %w[orders province]
+  end
 end
