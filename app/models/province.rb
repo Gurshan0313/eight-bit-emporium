0df751ca3f9d7709377e044ffec09e660bf6a755
Gurshan Sekhon <gsekhon41@rrc.ca>
2026-04-08 12:51:23 -0500

    Add ransackable_attributes and associations to Province for ActiveAdmin

--- app/models/province.rb
+++ app/models/province.rb
@@ -4,4 +4,14 @@ class Province < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   validates :abbreviation, presence: true, length: { is: 2 }
   validates :gst, :pst, :hst, numericality: { greater_than_or_equal_to: 0 }
+
+  # Allow Ransack to search these associations (for Active Admin filters)
+  def self.ransackable_associations(auth_object = nil)
+    ["users"]
+  end
+
+  # Allow Ransack to search these attributes (columns)
+  def self.ransackable_attributes(auth_object = nil)
+    ["name", "abbreviation", "gst", "pst", "hst", "created_at", "updated_at"]
+  end
 end
